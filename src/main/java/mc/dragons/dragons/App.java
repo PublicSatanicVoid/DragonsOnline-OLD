c8f51000ea4cde445a7bd70fd14d342331be259d
UniverseCraft <55899222+UniverseCraft@users.noreply.github.com>
2020-05-09 23:00:22 -0500

    Initial commit

---
+++ src/main/java/mc/dragons/dragons/App.java
@@ -0,0 +1,13 @@
+package mc.dragons.dragons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
